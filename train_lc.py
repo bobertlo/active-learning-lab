8d31d15474e3c6ebc4f497dfe347d1c876e4bb54
Robert Lowry <bobertlo@gmail.com>
2022-10-03 23:56:37 -0500

    add least confidence model

--- train_lc.py
+++ train_lc.py
@@ -8,6 +8,7 @@ from sklearn.utils import resample
 import pandas as pd
 import os
 import json
+import numpy as np
 
 if __name__ == "__main__":
     params = load_params()
@@ -17,14 +18,16 @@ if __name__ == "__main__":
     print(f'train: X{X.shape} y{y.shape}')
     print(f'test:  X{X_test.shape} y{y_test.shape}')
 
+    X_train, X_reserve, y_train, y_reserve = prepare_training_set_random(X, y, train_size=500)
+
     test_metrics = []
     training_logs = []
-    for train_size in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]:
-        print(f"\n### Running Training Size {train_size}:")
-        X_train, X_valid, y_train, y_valid = prepare_training_set_random(X, y, train_size=train_size)
-        if len(X_valid) > train_size:
-            X_valid, y_valid = resample(X_valid, y_valid, n_samples=train_size, stratify=y_valid)
+    for i in range(15):
+        train_size = 100 + (i * 100)
+        X_valid = X_reserve[:1000]
+        y_valid = y_reserve[:1000]
 
+        print(f"\n### Running Training Size {train_size}:")
         model, log = train_basic_cnn(X_train, y_train, X_valid, y_valid, params)
 
         test_scores = model.evaluate(X_test, y_test)
@@ -44,6 +47,19 @@ if __name__ == "__main__":
             "log": log.history,
         })
 
+        print("### Selecting next samples ...")
+        max_preds = model.predict(X_valid).max(axis=1)
+        sorted_preds = sorted(enumerate(max_preds), key=lambda x:x[1])
+        idxs = [x[0] for x in sorted_preds[:500]]
+
+        X_new = X_reserve[idxs]
+        y_new = y_reserve[idxs]
+        X_reserve = np.delete(X_reserve, idxs, 0)
+        y_reserve = np.delete(y_reserve, idxs, 0)
+
+        X_train = np.append(X_train, X_new, axis=0)
+        y_train = np.append(y_train, y_new, axis=0)
+
     sizes = []
     accs = []
     losses = []
@@ -59,7 +75,7 @@ if __name__ == "__main__":
             "train_size": x['train_size'],
             "loss": x['loss'],
         })
-    with open("plots/baseline_cnn_loss.json", "w") as outfile:
+    with open("plots/lc_cnn_loss.json", "w") as outfile:
         json.dump(loss_metrics, outfile)
 
     acc_metrics = []
@@ -68,5 +84,5 @@ if __name__ == "__main__":
             "train_size": x['train_size'],
             "acc": x['accuracy'],
         })
-    with open("plots/baseline_cnn_acc.json", "w") as outfile:
+    with open("plots/lc_cnn_acc.json", "w") as outfile:
         json.dump(acc_metrics, outfile)
