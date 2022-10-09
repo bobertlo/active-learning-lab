fc5068fd882bd68e756c3ad1919c3070d54ba5f2
Robert Lowry <bobertlo@gmail.com>
2022-10-09 18:48:48 -0500

    add metrics gathering to experiment runner

--- exp_run.py
+++ exp_run.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pandas as pd
 
 def run_cmd(cmd):
     print(f"### Running Command: {cmd}")
@@ -18,12 +20,38 @@ def run_experiment(expname, selector, seed, source_branch="main"):
         run_cmd(f"git commit -m 'run {expname}: {selector}{seed}'")
         run_cmd(f"git tag {expname}-{selector}{seed}")
 
+def load_metrics(method, seed):
+    sizes, accs, losses = [], [], []
+    with open ("plots/accuracy.json", "r") as infile:
+        acc_json = json.load(infile)
+    for x in acc_json:
+        accs.append(x['acc'])
+        sizes.append(x['train_size'])
+    with open ("plots/loss.json", "r") as infile:
+        loss_json = json.load(infile)
+    for x in loss_json:
+        losses.append(x['loss'])
+
+    df = pd.DataFrame({"size": sizes, "accuracy": accs, "loss": losses})
+    df['method'] = method
+    df['seed'] = seed
+    return df
+
 if __name__ == "__main__":
     source_branch = "main"
     exp_name = "mnist2"
 
+    metrics = None
     for seed in range(5):
         for method in ['random', 'lc', 'sm', 'ent']:
             run_experiment(exp_name, method, seed, source_branch=source_branch)
+            exp_metrics = load_metrics(method, seed)
+            if metrics is None:
+                metrics = exp_metrics
+            else:
+                metrics = metrics.append(exp_metrics)
 
     run_cmd(f'git checkout {source_branch}')
+
+    print("saving metrics to metrics.csv")
+    metrics.to_csv("metrics.csv")
