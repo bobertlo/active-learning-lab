57d3b919abf410d6566b57a05c35c5697f7fa81c
Robert Lowry <bobertlo@gmail.com>
2022-10-09 22:28:05 -0500

    refactor selection classes to use base class

--- selection.py
+++ selection.py
@@ -2,19 +2,24 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 
-class BaselineSelector:
+class Selector:
     def __init__(self, X, y, train_size=1000, seed=0):
         self.X_train, self.X_reserve, self.y_train, self.y_reserve = train_test_split(
             X, y, train_size=train_size, random_state=seed, stratify=y
         )
-
+    
     def print(self):
-        print(f"train:  X{self.X_train.shape} y{self.y_train.shape}")
+        print(f"train:   X{self.X_train.shape} y{self.y_train.shape}")
         print(f"reserve: X{self.X_reserve.shape} y{self.y_reserve.shape}")
-
+    
     def get(self):
         return self.X_train, self.X_reserve, self.y_train, self.y_reserve
+    
+    def select(self, model, size=1000):
+        raise NotImplementedError
 
+    
+class BaselineSelector(Selector):
     def select(self, model, size=1000):
         X_new, self.X_reserve, y_new, self.y_reserve = train_test_split(
             self.X_reserve,
@@ -32,19 +37,7 @@ class BaselineSelector:
 
         return self.X_train, self.X_reserve, self.y_train, self.y_reserve
 
-class LeastConfidenceSelector:
-    def __init__(self, X, y, train_size=1000, seed=0):
-        self.X_train, self.X_reserve, self.y_train, self.y_reserve = train_test_split(
-            X, y, train_size=train_size, random_state=seed, stratify=y
-        )
-    
-    def print(self):
-        print(f"train:  X{self.X_train.shape} y{self.y_train.shape}")
-        print(f"reserve: X{self.X_reserve.shape} y{self.y_reserve.shape}")
-    
-    def get(self):
-        return self.X_train, self.X_reserve, self.y_train, self.y_reserve
-
+class LeastConfidenceSelector(Selector):
     def select(self, model, size=1000):
         max_preds = model.predict(self.X_reserve).max(axis=1)
         sorted_preds = sorted(enumerate(max_preds), key=lambda x:x[1])
@@ -60,19 +53,7 @@ class LeastConfidenceSelector:
 
         return self.X_train, self.X_reserve, self.y_train, self.y_reserve
 
-class SmallestMarginSelector:
-    def __init__(self, X, y, train_size=1000, seed=0):
-        self.X_train, self.X_reserve, self.y_train, self.y_reserve = train_test_split(
-            X, y, train_size=train_size, random_state=seed, stratify=y
-        )
-    
-    def print(self):
-        print(f"train:  X{self.X_train.shape} y{self.y_train.shape}")
-        print(f"reserve: X{self.X_reserve.shape} y{self.y_reserve.shape}")
-    
-    def get(self):
-        return self.X_train, self.X_reserve, self.y_train, self.y_reserve
-
+class SmallestMarginSelector(Selector):
     def select(self, model, size=1000):
         def margin(x):
             sx = sorted(x, reverse=True)
@@ -94,19 +75,7 @@ class SmallestMarginSelector:
 
         return self.X_train, self.X_reserve, self.y_train, self.y_reserve
 
-class MaxEntropySelector:
-    def __init__(self, X, y, train_size=1000, seed=0):
-        self.X_train, self.X_reserve, self.y_train, self.y_reserve = train_test_split(
-            X, y, train_size=train_size, random_state=seed, stratify=y
-        )
-    
-    def print(self):
-        print(f"train:  X{self.X_train.shape} y{self.y_train.shape}")
-        print(f"reserve: X{self.X_reserve.shape} y{self.y_reserve.shape}")
-    
-    def get(self):
-        return self.X_train, self.X_reserve, self.y_train, self.y_reserve
-
+class MaxEntropySelector(Selector):
     def select(self, model, size=1000):
         def entropy(x):
             def h(x):
